20a504bffd129fb2043bd67037be400c9b316b34
Paulo Rabelo <paulorabelo@hotmail.com>
2022-02-21 00:13:49 -0300

    adicionado variaveis e SeekBar Listener

--- app/src/main/java/br/com/mrgsoft/calculadoradegorjetas/MainActivity.java
+++ app/src/main/java/br/com/mrgsoft/calculadoradegorjetas/MainActivity.java
@@ -3,12 +3,49 @@ package br.com.mrgsoft.calculadoradegorjetas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editValor;
+    private TextView textPorcentagem;
+    private TextView textGorgeja;
+    private TextView textTotal;
+    private SeekBar seekBarGorjeta;
+
+    private double porcentagem = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editValor       = findViewById(R.id.editValor);
+        textPorcentagem = findViewById(R.id.textPorcentagem);
+        textGorgeja     = findViewById(R.id.textGorjeta);
+        textTotal       = findViewById(R.id.textTotal);
+        seekBarGorjeta  = findViewById(R.id.seekBarGorgeta);
+
+        //Listener SeekBar
+        seekBarGorjeta.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+                porcentagem = i;
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
